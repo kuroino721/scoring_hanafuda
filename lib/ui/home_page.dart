795c3a7c049a0944447bd6d82b324a68221f6b40
kuroino721 <kuroino721@gmail.com>
2019-12-20 19:14:52 +0900

    main.dartを細分化

---
+++ lib/ui/home_page.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'dart:math' as math;
+
+class HomePage extends StatelessWidget {
+  HomePage({Key key, this.title}) : super(key: key);
+  final String title;
+
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: new AppBar(
+        title: Text(title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: <Widget>[
+            RaisedButton(
+              onPressed: () => Navigator.pushNamed(context, "/koikoi"),
+              child: new Text('こいこい'),
+            ),
+            RaisedButton(
+//              onPressed: () => Navigator.pushNamed(context, "/hachihachi"),
+              onPressed: null,
+              child: new Text('はちはち'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
